9688560634dd4bb8091ff14de563f0814f6011c9
Oleksandr Simochko <vhsb721@gmail.com>
2026-05-11 13:09:59 +0200

    fix: order str format

--- db/models.py
+++ db/models.py
@@ -66,8 +66,7 @@ class Order(models.Model):
         ordering = ["-created_at"]
 
     def __str__(self) -> str:
-        return f"<Order: {self.created_at}>"
-
+        return str(self.created_at)
 
 class Ticket(models.Model):
     movie_session = models.ForeignKey(
